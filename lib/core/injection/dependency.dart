936f484e65dc49cfe05f06e08897a09ad2a701d3
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-02-08 09:11:52 +0100

    Inject Post dependencies

--- lib/core/injection/dependency.dart
+++ lib/core/injection/dependency.dart
@@ -1,26 +1,56 @@
 import 'package:dio/dio.dart';
+import 'package:front/core/cache/post_comment.dart';
 import 'package:front/core/networking/api.service.dart';
 import 'package:front/core/networking/dio.factory.dart';
+import 'package:front/core/networking/info.dart';
 import 'package:front/features/login/data/repository/login.repo.dart';
 import 'package:front/features/login/logic/login_cubit.dart';
+import 'package:front/features/posts/data/repositories/post.imp.dart';
+import 'package:front/features/posts/data/source/post.local.dart';
+import 'package:front/features/posts/data/source/post.remote.dart';
+import 'package:front/features/posts/domain/repositories/post.dart';
+import 'package:front/features/posts/logic/post/post_cubit.dart';
 import 'package:front/features/signup/data/repo/signup.repo.dart';
 import 'package:front/features/signup/logic/signup_cubit.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final getIt = GetIt.instance;
+part 'post.dependencies.dart';
+
+final locator = GetIt.instance;
 
 Future<void> setupDependencies() async {
+  //**                                                      **\\
+  //*                   Global dependencies                  *\\
+  //**                                                      **\\
+
   //Dio & ApiService
-  Dio dio = DioFactory.getDio();
-  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+  locator.registerLazySingleton<Dio>(() => DioFactory.getDio());
+  locator.registerLazySingleton<ApiService>(() => ApiService(locator<Dio>()));
+
+  //Shared preferences
+  locator.registerLazySingletonAsync(
+      () async => await SharedPreferences.getInstance());
+
+  //Internet connection checker
+  locator
+      .registerLazySingleton(() => InternetConnectionChecker.createInstance());
+
+  //**                                                      **\\
+  //*                   Feature  dependencies                *\\
+  //**                                                      **\\
 
   //login
-  getIt.registerLazySingleton<LoginRepository>(() => LoginRepository(getIt()));
-  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
+  locator
+      .registerLazySingleton<LoginRepository>(() => LoginRepository(locator()));
+  locator.registerFactory<LoginCubit>(() => LoginCubit(locator()));
 
   //SignUp
-  getIt.registerLazySingleton<SignupRepository>(() => SignupRepository(getIt()));
-  getIt.registerFactory<SignupCubit>(() => SignupCubit(getIt()));
+  locator.registerLazySingleton<SignupRepository>(
+      () => SignupRepository(locator()));
+  locator.registerFactory<SignupCubit>(() => SignupCubit(locator()));
 
-  
+  //post
+  await setupPostDependencies();
 }
