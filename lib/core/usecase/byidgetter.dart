19cfb52197bafffd9ed20c9f658959e83b705c36
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-02-25 00:42:39 +0100

    add user to ByIdGetter

--- lib/core/usecase/byidgetter.dart
+++ lib/core/usecase/byidgetter.dart
@@ -1,1 +1,14 @@
-class ByIdGetter {}
+import 'package:front/core/injection/dependency.dart';
+import 'package:front/features/auth/domain/enitities/user.dart';
+import 'package:front/features/auth/domain/usecases/get.profile.dart';
+
+class ByIdGetter {
+  final String id;
+
+  ByIdGetter({required this.id});
+  
+  Future<User?> get user async =>
+      (await locator<GetProfileUseCase>()(id)).mapOrNull(
+        sucess: (value) => value.data,
+      );
+}
